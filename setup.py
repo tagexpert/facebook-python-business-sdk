051f81538e482001af1c702a1b550d3fb06d2971
Yoan Yahemdi <yoan@tag-expert.com>
2022-09-25 12:19:24 +0100

    change packagename to tefb

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ this_dir = os.path.dirname(__file__)
 readme_filename = os.path.join(this_dir, 'README.md')
 requirements_filename = os.path.join(this_dir, 'requirements.txt')
 
-PACKAGE_NAME = 'fb'
+PACKAGE_NAME = 'te_fb'
 PACKAGE_VERSION = '15.0.0'
 PACKAGE_AUTHOR = 'Facebook'
 PACKAGE_AUTHOR_EMAIL = ''
